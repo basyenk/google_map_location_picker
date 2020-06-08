deb12045ff27af4cbf19bdfa5caae9819e768975
Habibullah Aminuddin <admin@basyenk.info>
2020-06-08 16:04:07 +0700

    Add icon color

--- lib/src/map.dart
+++ lib/src/map.dart
@@ -404,7 +404,7 @@ class _MapFabs extends StatelessWidget {
 
   final VoidCallback onToggleMapTypePressed;
   final VoidCallback onMyLocationPressed;
-  final Color iconColor;
+  Color iconColor;
 
   @override
   Widget build(BuildContext context) {
